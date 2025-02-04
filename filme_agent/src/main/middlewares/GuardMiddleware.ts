bd1c2d4380c91aca589bb0c27076838d52f5f73d
ItaloF532 <italo.francisco532@gmail.com>
2025-02-04 08:57:54 -0300

    refactor: adjust project to be a monorepo

---
+++ filme_agent/src/main/middlewares/GuardMiddleware.ts
@@ -0,0 +1,6 @@
+export class GuardMiddleware {
+  // TODO: implements guard middleware to filter
+  // innaproppriate messages
+  // TODO: criar micro serviço para executar as perguntas.
+  // TODO: testar todas as perguntas
+}
