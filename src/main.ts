70689fdfa645f9cbae9b2851477d276967f9a0e1
ItaloF532 <italo.francisco532@gmail.com>
2025-02-03 21:24:38 -0300

    feat: create chat controller

--- src/main.ts
+++ src/main.ts
@@ -1,17 +1,22 @@
+import { Request, Response } from 'express';
 import { ENVIRONMENT } from './constants/environment';
+import { ChatController } from './main/controllers/ChatController';
 
 const express = require('express');
 const cors = require('cors');
 
 const app = express();
-const port = ENVIRONMENT.PORT || 3000;
+const port = ENVIRONMENT.PORT || 8080;
 
 app.use(cors());
 
-app.get('/chat', (req, res) => {
-  res.send('Olá Mundo!');
+app.get('/chat', async (req: Request, res: Response) => {
+  const { message } = req.query as { message: string };
+  const teste = await new ChatController().handleChat(message);
+
+  res.send(teste);
 });
 
-app.listen(port, () => {
+app.listen(port, async () => {
   console.log(`Running on: ${port}`);
 });
---
+++ src/main/controllers/ChatController.ts
@@ -0,0 +1,38 @@
+import {
+  GroqGemmaProvider,
+  MessageType,
+} from '../../infra/providers/groq/providers/GroqGemmaProvider';
+import { GroqMixtralProvider } from '../../infra/providers/groq/providers/GroqMixtralProvider';
+import { TheMovieDbProvider } from '../../infra/providers/theMovie/providers/TheMovideDbProvider';
+
+export class ChatController {
+  protected theMovieDbProvider = new TheMovieDbProvider();
+  protected groqGemmaProvider = new GroqGemmaProvider();
+  protected groqMixtralProvider = new GroqMixtralProvider();
+
+  async handleChat(message: string): Promise<string> {
+    const checkMessage = await this.groqGemmaProvider.getMessageType(message);
+
+    if (checkMessage === MessageType.RECOMMENDATION) {
+      console.time('getRecomendations');
+      const trendingAndMostLikedFilmes =
+        await this.theMovieDbProvider.getRecomendations();
+      console.timeEnd('getRecomendations');
+
+      return await this.groqMixtralProvider.getRecommendations(
+        JSON.stringify(trendingAndMostLikedFilmes),
+        message,
+      );
+    }
+
+    const filmTitle = await this.groqGemmaProvider.getTitleFromMessage(message);
+    const searchedFilme = await this.theMovieDbProvider.getFilmByName(
+      filmTitle,
+    );
+
+    return await this.groqGemmaProvider.message(
+      JSON.stringify(searchedFilme),
+      message,
+    );
+  }
+}
