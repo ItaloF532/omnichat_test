c86acbf522469c8198b974c3f71ec28de8eb6f97
ItaloF532 <italo.francisco532@gmail.com>
2025-02-03 21:23:49 -0300

    feat: create groq provide impl with gemma

---
+++ src/infra/providers/groq/providers/GroqGemmaProvider.ts
@@ -0,0 +1,36 @@
+import { GroqBaseAPI } from '../GroqBaseApi';
+import { checkMessageTypePrompt } from '../prompts/CheckMessageTypePrompt';
+import { extractTitleFromMessagePrompt } from '../prompts/ExtractTitleFromMessagePrompt';
+import { specificFilmPrompt } from '../prompts/SpecificFilmPrompt';
+
+export enum MessageType {
+  RECOMMENDATION = 'RECOMMENDATION',
+  FILM = 'FILM',
+}
+
+export class GroqGemmaProvider extends GroqBaseAPI {
+  public async message(
+    enhancedFilmInformations: string,
+    message: string,
+  ): Promise<string> {
+    return await this.sendMessage(
+      specificFilmPrompt(enhancedFilmInformations, message),
+    );
+  }
+
+  public async getTitleFromMessage(message: string): Promise<string> {
+    return await this.sendMessage(extractTitleFromMessagePrompt(message));
+  }
+
+  public async getMessageType(message: string): Promise<MessageType> {
+    const msgType = await this.sendMessage(checkMessageTypePrompt(message));
+
+    switch (msgType.trim().toLowerCase()) {
+      case 'recomendacao':
+        return MessageType.RECOMMENDATION;
+      case 'filme':
+      default:
+        return MessageType.FILM;
+    }
+  }
+}
